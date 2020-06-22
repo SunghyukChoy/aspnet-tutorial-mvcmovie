7c29881faa13483927fcc761ca687e29ee079ed0
Sunghyuk Choi <likeaglow@gmail.com>
2020-06-22 18:50:03 +0900

    개요, 시작, 컨트롤러 추가, 보기 추가, 모델 추가

---
+++ Practice.FirstWebApp/Models/Post.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practice.FirstWebApp.Models
+{
+    public class Post
+    {
+        
+    }
+}
