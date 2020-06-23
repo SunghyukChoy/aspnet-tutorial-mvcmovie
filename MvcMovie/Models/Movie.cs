4c5abaea49c5840ff10a1bb28f4d7b9b52e180eb
Sunghyuk Choi <likeaglow@gmail.com>
2020-06-23 16:51:19 +0900

    ASP.NET Core의 컨트롤러 메서드 및 보기

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,9 +13,12 @@ namespace MvcMovie.Models
         public int Id { get; set; }
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }        
     }
 }
